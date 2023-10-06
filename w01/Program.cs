3162548ee1ab9e17f09a0baedbbac685d887318a
Aydin Secer <aydinsecer@gmail.com>
2023-10-06 10:49:23 +0300

    Add project files.

---
+++ w01/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace w01 //packace, library, coverage code and object location
+{
+    internal static class Program
+    {
+        //starting or entry point method/function
+        private static int _avAlue = 1;
+        
+        static void Main(string[] args)
+        {
+            int b = 1+3;
+            Console.WriteLine("Hello world!!!");//printf in c
+        }
+    }
+}
+
+// Tokens:
+// 01- keywords
+// 02- identifiers, mus be obey the rules of variable definitions
+// 03- symbols
+// 04- operators
+// 05- Literals
+//       numerical
+//       string
+
+
+

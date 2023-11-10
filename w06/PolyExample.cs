02a30b96607f496bade3cc29736de93eeb3866f0
Aydin Secer <aydinsecer@gmail.com>
2023-11-10 11:59:49 +0300

    u

--- w06/PolyExample.cs
+++ w06/PolyExample.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace w06
 {
-
     //polymorphism
+    //valid for regular classes
     //valid for abstract classes also
+    //valid for interfaces.
     class A
     {
         public virtual void m1()
         {
             Console.WriteLine("A.m1()");
         }
-
     }
 
     class B:A

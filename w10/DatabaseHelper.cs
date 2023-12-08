d2f046ad4c91fd73fd4b86e939e87506c96376c9
Aydin Secer <aydinsecer@gmail.com>
2023-12-08 11:42:48 +0300

    u

---
+++ w10/DatabaseHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w10
+{
+    /*
+       Events in C#,Java are a way to provide a mechanism for communication between objects. They are used in situations where an object needs to notify other objects that something has happened. In C# and .NET, Java, events are based on the delegate model, which provides a way to reference methods with a specific set of arguments and return type.     
+     */
+
+    /*
+        Delegate Declaration: Before declaring an event, you need to declare a delegate that defines the signature of the event handler method. This delegate will be used to type the event.
+
+        Event Declaration: An event is declared using the event keyword followed by the delegate type. This event can then be subscribed to by event handlers (methods) that match the delegate signature.
+
+        Event Subscription: Other objects or classes subscribe to the event by attaching an event handler method to the event. This is usually done using the += operator.
+
+        Raising the Event: When the event occurs (i.e., when the specific condition is met), the event is raised using the event keyword. All subscribed event handlers are then called.
+
+        Event Unsubscription: Event handlers can be unsubscribed using the -= operator.
+    */
+
+    public class DatabaseHelper
+    {
+        //Step 1
+        // public delegate void BackupCompletedDelegate(object sender,EventArgs e);
+        public delegate void BackupCompletedDelegate();
+        //Step 2
+        public event BackupCompletedDelegate BackupCompleted;
+
+        //event 2
+        public delegate void BackupStartedDelegate();
+        public event BackupStartedDelegate BackupStarted;
+        //event 3
+        public delegate void DatabaseShutDownDelegate();
+        public event DatabaseShutDownDelegate DatabaseShutDownCompleted;
+
+        public void GetBackup()
+        {
+            //backup processes....
+            //...
+
+            Console.WriteLine("Backup Completed");
+            OnBackupCompleted();
+        }
+
+        public void StartBackup()
+        {
+            //backup start processes....
+            //...
+
+            Console.WriteLine("Backup Started");
+            OnBackupStarted();
+        }
+
+        public void ShutDown()
+        {
+            //ShutDown processes....
+            //...
+
+            Console.WriteLine("ShutDown Completed");
+            OnDatabaseShutDownCompleted();
+        }
+
+
+        protected virtual void OnBackupCompleted()
+        {
+            BackupCompleted?.Invoke();
+        }
+
+        protected virtual void OnBackupStarted()
+        {
+            BackupStarted?.Invoke();
+        }
+
+        protected virtual void OnDatabaseShutDownCompleted()
+        {
+            DatabaseShutDownCompleted?.Invoke();
+        }
+    }
+}
---
+++ w10/Program.cs
@@ -0,0 +1,263 @@
+﻿
+
+namespace w10
+{
+
+    class Program
+    {
+        //method pointer for appropriate signature of methods.
+
+        public delegate void MyDelegate();
+
+        public delegate int MathOperation(int x, int y);
+
+        public delegate T MathGlobalOperation<T>(T x, T y); //generic delegate
+
+        static void Main(string[] args)
+        {
+            //MyDelegate methods = MethodOne; //callback assignment
+            //methods += MethodTwo;//binding callback method to delegate
+
+            //methods();
+
+            ////FIRST USAGE
+            //MathOperation toplama = Add;
+            //MathOperation cikarma = Subtract;
+            //MathOperation carpma = Multiply;
+            //MathOperation bolme = Divide;
+
+            //Console.WriteLine(toplama(1,2));
+            //Console.WriteLine(cikarma(1, 2));
+            //Console.WriteLine(carpma(1, 2));
+            //Console.WriteLine(bolme(10, 2));
+
+            ////SECOND USAGE //MULTICAST DELEGATES
+            //MathOperation mathOperations = Add;
+            //mathOperations += Subtract;  //+=  binding event or delegate
+            //mathOperations += Multiply;//binding
+            //mathOperations += Divide;//binding
+            //mathOperations -= Subtract; //unbinding op
+
+
+
+            //mathOperations(3, 1);
+
+
+            //SelectOperation(50,5,Multiply);
+            //MathOperation m1 = Divide;
+            //SelectOperation(50, 5, m1);
+            //Console.WriteLine(GetOp(1)(60,12));
+
+            // MathOperation m1 = Multiply;
+            //// m1(1.1f, 2); //err
+
+            ////generic delegate callback assignment
+            // MathGlobalOperation<double> m2 = Multiply;
+            // Console.WriteLine(m2(1.1f, 2));
+
+            //Action type delegate/ with no input and no output
+            //Action myAction = () =>
+            //{
+            //    //method body
+            //};//lambda expression (=>)
+            //Action myPrint = () =>
+            //{
+            //    Console.WriteLine("Anythging");
+            //};
+
+            //myPrint();
+
+            ////Action type generic delegate/ with input and no output
+            //Action<int, int> add = (x, y) =>
+            //{
+            //    Console.WriteLine(x + y);
+            //};
+
+            //add(1, 2);
+
+            //Action<int, int> add2 = Topla;
+
+            //Func generic delegate//with input param, and return output
+
+            //Func<inputs..., output> 
+
+            //Func<int, int, int> add3 = Add;
+
+            //add3 += Divide;//multicast
+
+            //Func<double, double, double> add4 = Add;
+
+            //Func<int> f1 = () => 5;//simple lambda exp. //directly return any value in a single line, don't need return keyword
+
+            //Func<int> f2 = () => //complex lambda exp.
+            //{
+            //    //elaborate ops.
+            //    return 5;
+            //};
+
+            //Console.WriteLine(AdvancedMath<int>(Add, 2, 3));
+
+            //Console.WriteLine(AdvancedMath<double>(Add, 2.3, 3));
+
+            //Console.WriteLine(AdvancedMath(
+            //    (int x, int y) =>
+            //    {
+            //        return x * y + 2;
+
+
+            //    }, 10, 5));
+
+            //Console.WriteLine(AdvancedMath((int x, int y) => {return x + y * 2 + 4; }, 10, 5));
+
+            //Console.WriteLine(AdvancedMath((double x, double y) => { return x + y / 2 + 4; }, 10, 53));
+
+
+            DatabaseHelper databaseHelper = new DatabaseHelper();
+            //...some other ops.
+            databaseHelper.BackupStarted += DatabaseHelper_BackupStarted;
+            databaseHelper.BackupCompleted += DatabaseHelperOnBackupCompleted; //event binding/ subscribe
+            databaseHelper.BackupCompleted += DatabaseHelperOnBackupCompleted2; //event binding/ subscribe
+                                                                                //DatabaseHelperOnBackupCompleted = handler method, event handler method, callback, delegate
+            databaseHelper.DatabaseShutDownCompleted += DatabaseHelper_DatabaseShutDownCompleted; ;
+
+            databaseHelper.StartBackup();
+            //backup 12.00
+            databaseHelper.GetBackup();
+            //....
+            //15.00
+            databaseHelper.GetBackup();
+
+            databaseHelper.ShutDown();
+
+        }
+
+        private static void DatabaseHelper_DatabaseShutDownCompleted()
+        {
+            Console.WriteLine("Database Shut down completed, notification SMS has been sent to the Administrator");
+        }
+
+        private static void DatabaseHelper_BackupStarted()
+        {
+            Console.WriteLine("Database back up started, notification SMS has been sent to the Administrator");
+        }
+
+        private static void DatabaseHelperOnBackupCompleted2()
+        {
+            //this is subscription method and whenever event triggered, this method handler will be executed.
+            //..extra ops......
+
+            Console.WriteLine("Database backed up, notification SMS has been sent to the Administrator");
+        }
+
+        private static void DatabaseHelperOnBackupCompleted()
+        {
+            //this is subscription method and whenever event triggered, this method handler will be executed.
+            //..extra ops......
+
+            Console.WriteLine("Database backed up, notification email has been sent to the Administrator");
+        }
+
+        //generic method with generic input method
+        static T AdvancedMath<T>(Func<T, T, T> f, T x, T y)
+        {
+            return f(x, y);
+        }
+
+
+
+        static void Topla(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+
+        //THIRD USAGE
+        static void SelectOperation(int x, int y, MathOperation op)//callback parameter
+        {
+            Console.WriteLine(op(x, y));
+        }
+
+        //FOURTH USAGE
+        //callback return
+        static MathOperation GetOp(int op)
+        {
+            if (op == 1)
+            {
+                return Add;
+            }
+            if (op == 2)
+            {
+                return Subtract;
+            }
+
+            if (op == 3)
+            {
+                return Multiply;
+            }
+
+            return Divide;
+        }
+
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        static int Divide(int x, int y)
+        {
+            if (y != 0)
+                return x / y;
+            else
+            {
+                Console.WriteLine("Division by 0 exception");
+                return 0;
+            }
+        }
+        static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        static double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+        static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        static double Divide(double x, double y)
+        {
+            if (y != 0)
+                return x / y;
+            else
+            {
+                Console.WriteLine("Division by 0 exception");
+                return 0;
+            }
+        }
+
+        public static void MethodOne()
+        {
+            Console.WriteLine("MethodOne called.");
+        }
+
+        public static void MethodTwo()
+        {
+            Console.WriteLine("MethodTwo called.");
+        }
+
+
+
+
+    }
+
+
+}
+
